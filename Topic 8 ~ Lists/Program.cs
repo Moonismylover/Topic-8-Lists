77c980877292cfc283cc4d4b8c63062bff8fcf87
Moonismylover <Angelpreetthind9@gmail.com>
2026-03-26 13:19:27 -0400

    Add project files.

---
+++ Topic 8 ~ Lists/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Topic_8___Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Topic 8 ~ Lists";
+
+            // A List allows us to store multiple variables of the same datatype. 
+            // Ex. String name1, name2, name;
+
+            List<string> names = new List<string> { "Trinity", "Neo", "Morpheus", "Smith" };
+            Console.WriteLine(names[1] + " is 'The One'.");
+            Console.WriteLine(names[3] + " is the bad guy.");
+            Console.WriteLine("There are " + names.Count + " names in my List.");
+            names[3] = "Agent Smith";
+            Console.WriteLine(names[3] + " is the bad guy.");
+            Console.ReadLine(); 
+
+            /**
+            
+            1) What does the number mean in the square brackets names[1]?? (Referred to as index)
+                
+
+            **/
+        }
+    }
+}
