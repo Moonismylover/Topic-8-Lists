544d68960719cdb1f14518af7f2de9e38fbfa5a7
Moonismylover <Angelpreetthind9@gmail.com>
2026-03-29 16:51:52 -0400

    qwertyuiop

--- Topic 8 ~ Lists/Program.cs
+++ Topic 8 ~ Lists/Program.cs
@@ -9,20 +9,31 @@
             // A List allows us to store multiple variables of the same datatype. 
             // Ex. String name1, name2, name;
 
-            List<string> names = new List<string> { "Trinity", "Neo", "Morpheus", "Smith" };
+            List<string> names = new List<string> { "Trinity", "Neo", "Morpheus", "Smith", "Sliver"};
             Console.WriteLine(names[1] + " is 'The One'.");
             Console.WriteLine(names[3] + " is the bad guy.");
+            Console.WriteLine(names[4] + " is the best!");
             Console.WriteLine("There are " + names.Count + " names in my List.");
             names[3] = "Agent Smith";
             Console.WriteLine(names[3] + " is the bad guy.");
-            Console.ReadLine(); 
+            names[names.Count - 1] = "Cypher";
+            Console.WriteLine(names[names.Count - 1] + " is the best!");
+
+            Console.ReadLine();
 
             /**
             
-            1) What does the number mean in the square brackets names[1]?? (Referred to as index)
+            1) What does the number mean in the square brackets: names[1]?? (Referred to as index)
                It represents the order of the numbers in the sequence of the list.
-            2) 
-
+            2) How could you print out the first name (Trinity) in names?
+               Console.WriteLine(names[0]);
+            3) When the List is declared, add another name.
+            4) What do you think names.Count does? 
+               It counts the number of names in the list. 
+            5) Change the name stored in the last position to "Cypher" and then print it. 
+               Challenge: Use names.Count to change the last name instead of using the index number.
+               
+            6) 
             **/
         }
     }
